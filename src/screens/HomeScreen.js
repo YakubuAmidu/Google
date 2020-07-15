768bcb97166a51164075e866a68aff42aed2ebd1
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-07-15 00:30:42 -0700

    Commiting changes

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -46,29 +46,46 @@ export default class Google extends Component {
            marginTop: 20,
            color: '#808080'
          },
-  
-  textStyle2: {
-    backgroundColor: '#DFDFDF',
-    color: '#808080',
-    fontWeight: 'bold',
-    marginRight: 10,
-    paddingTop: 10,
-    paddingBottom: 10,
-    paddingRight: 15,
-    paddingLeft: 15,
-    marginBottom: 20
-  },
-  textStyle3: {
-    color: '#808080',
-    fontWeight: 'bold'
-  },
-  viewStyle2: {
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'center'
-  },
-  textStyle4: {
-    color: 'blue',
-    marginLeft: 5
-  }
-})
+         textInputStyle: {
+           fontSize: 20,
+           borderWidth: 1,
+           borderColor: '#ddd',
+           paddingTop: 10,
+           paddingBottom: 10,
+           paddingRight: 10,
+           marginLeft: 20,
+           marginRight: 20,
+           marginTop: 20,
+           textAlign: 'right',
+         },
+         viewStyle1: {
+           flex: 1,
+           flexDirection: 'row',
+           justifyContent: 'center',
+           marginTop: 20
+         },
+         textStyle2: {
+           backgroundColor: '#DFDFDF',
+           color: '#808080',
+           fontWeight: 'bold',
+           marginRight: 10,
+           paddingTop: 10,
+           paddingBottom: 10,
+           paddingRight: 15,
+           paddingLeft: 15,
+           marginBottom: 20
+         },
+         textStyle3: {
+           color: '#808080',
+           fontWeight: 'bold'
+         },
+         viewStyle2: {
+           flex: 1,
+           flexDirection: 'row',
+           justifyContent: 'center'
+         },
+         textStyle4: {
+           color: 'blue',
+           marginLeft: 5
+         }
+       })
