ac138807379b2f61c1e0e8b34b9e299957b78417
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-07-15 00:22:25 -0700

    Commiting changes

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,6 +1,3 @@
-import React, { Component } from 'react';
-import { View, Text, StyleSheet, ScrollView, TextInput } from 'react-native';
-import { FontAwesome } from '@expo/vector-icons';
 
 
 export default class Google extends Component {
